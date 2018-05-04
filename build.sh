1362fb9cd1ed912c4172eaa2fcf5798deb06f6bc
olexandr1712 <wersus99422@gmail.com>
2018-05-04 13:04:05 +0300

    Export ARCH=arm and Linaro 6.3.1 toolchain

--- build.sh
+++ build.sh
@@ -2,14 +2,17 @@
 
 rm ./arch/arm/boot/zImage*
 
+export CROSS_COMPILE=~/gcc-linaro-6.3.1/bin/arm-eabi-
 make clean
 make mrproper
+export ARCH=arm
 make fusion3_dogo_defconfig
 make zImage
 mv ./arch/arm/boot/zImage ./anykernel/zImage.dogo
 
 make clean
 make mrproper
+export ARCH=arm
 make fusion3_odin_defconfig
 make zImage
 mv ./arch/arm/boot/zImage ./anykernel/zImage.odin
@@ -17,6 +20,7 @@ cp ./arch/arm/boot/dtb* ./anykernel/
 
 make clean
 make mrproper
+export ARCH=arm
 make fusion3_yuga_defconfig
 make zImage
 mv ./arch/arm/boot/zImage ./anykernel/zImage.yuga
